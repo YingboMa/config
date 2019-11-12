b28ef92b9bbae56d7c5428797b6819aff1ae63a0
Shashi Gowda <shashigowda91@gmail.com>
2019-11-12 05:35:31 +0000

    emoji

--- dwmstatus/config.h
+++ dwmstatus/config.h
@@ -5,21 +5,23 @@
 #define SYSBRIGHT "/sys/class/backlight/intel_backlight/brightness"
 #define SYSBRIGHTMAX "/sys/class/backlight/intel_backlight/max_brightness"
 
-#define LEFTSEPARATOR " ["
-#define RIGHTSEPARATOR "] "
+//#define BEGINSEPARATOR " "
+//#define ENDSEPARATOR " "
+#define LEFTSEPARATOR " "
+#define RIGHTSEPARATOR " "
 #define AUDIOFS "🔊 %ld%%"
-#define TIMEFS "%m-%d %H:%M:%S"
-#define RAMFS "RAM %.2fM"
-#define BRIGHTFS "Brightness %ld%%"
-#define TEMPFS "%.0lf°C"
-#define CPUFS "CPU %2.0f%%"
-#define BATFS "Battery %d%%[%c]"
+#define TIMEFS "%d/%m %H:%M"
+#define RAMFS "🧠 %.0fM"
+#define BRIGHTFS "🔆 %ld%%"
+#define TEMPFS "🌡 %.0lfC"
+#define CPUFS "💻%2.0f%%"
+#define BATFS "🔋 %d%%%c"
 
 int (*const functab[])(char*, size_t) = {
     sep_left, audio, sep_right, sep_left, brightness, sep_right,
     sep_left, mem,   sep_right, sep_left, temp,       sep_right,
     sep_left, cpu,   sep_right, sep_left, bat,        sep_right,
-    sep_left, date,  sep_right,
+    sep_left, date,  sep_left,
 };
 
 int N_funcs = sizeof(functab) / sizeof(functab[0]);
