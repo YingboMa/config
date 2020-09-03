6d0dee785cd0e39ace2c9fc64c5afede6ec87140
Yingbo Ma <mayingbo5@gmail.com>
2020-09-02 21:20:09 -0400

    New XPS 13 update

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka Term Curly:style=Regular:pixelsize=20:antialias=true:autohint=true";
+static char *font = "Iosevka Term Slab:style=Regular:size=12:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
